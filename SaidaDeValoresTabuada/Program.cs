65497de30d1d478cefe5efb3c4665877d1d8bc9c
riquelmypereira <riqualmydasilva2009@gmail.com>
2026-04-22 14:49:26 -0300

    <obrigado>

---
+++ SaidaDeValoresTabuada/Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+Console.WriteLine("Tabuada do 9:");
+Console.Write(("9 * 1 = " + (9 * 1));
+Console.Write(("9 * 2 = " + (9 * 2));
+Console.Write(("9 * 3 = " + (9 * 3));
+Console.Write(("9 * 4 = " + (9 * 4));
+Console.Write(("9 * 5 = " + (9 * 5));
+Console.Write(("9 * 6 = " + (9 * 6));
+Console.Write(("9 * 7 = " + (9 * 7));
+Console.Write(("9 * 8 = " + (9 * 8));
+Console.Write(("9 * 9 = " + (9 * 9));
+Console.Write(("9 * 10 = " + (9 * 10));
