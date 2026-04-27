452ed5e6b283c445aa3a5a1f21368d82205a71ee
riquelmypereira <riqualmydasilva2009@gmail.com>
2026-04-27 14:27:31 -0300

    NotaDoAluno

--- SaidaDeValoresTabuada/Program.cs
+++ SaidaDeValoresTabuada/Program.cs
@@ -1,13 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
 Console.WriteLine("Tabuada do 9:");
-Console.Write(("9 * 1 = " + (9 * 1));
-Console.Write(("9 * 2 = " + (9 * 2));
-Console.Write(("9 * 3 = " + (9 * 3));
-Console.Write(("9 * 4 = " + (9 * 4));
-Console.Write(("9 * 5 = " + (9 * 5));
-Console.Write(("9 * 6 = " + (9 * 6));
-Console.Write(("9 * 7 = " + (9 * 7));
-Console.Write(("9 * 8 = " + (9 * 8));
-Console.Write(("9 * 9 = " + (9 * 9));
-Console.Write(("9 * 10 = " + (9 * 10));
+Console.WriteLine("9 * 1 = " + (9 * 1));
+Console.WriteLine("9 * 2 = " + (9 * 2));
+Console.WriteLine("9 * 3 = " + (9 * 3));
+Console.WriteLine("9 * 4 = " + (9 * 4));
+Console.WriteLine("9 * 5 = " + (9 * 5));
+Console.WriteLine("9 * 6 = " + (9 * 6));
+Console.WriteLine("9 * 7 = " + (9 * 7));
+Console.WriteLine("9 * 8 = " + (9 * 8));
+Console.WriteLine("9 * 9 = " + (9 * 9));
+Console.WriteLine("9 * 10 = " + (9 * 10));
